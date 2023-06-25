2a76ca1f78df483f40b60f6a90793ad4d1faec5c
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-06-25 10:02:21 +0530

    commit local changes

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-import os
-from flask import Flask, request, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # SQLite database file path
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+    country = db.Column(db.String(20))
+
+    def __init__(self, username, email, country="india"):
+        self.username = username
+        self.email = email
+        self.country = country
 
 
 @app.route('/')
-def main():
-    return render_template("index.html")
+def index():
+    # Perform database operations
+    return 'Database operations completed.'
 
-@app.route('/success', methods=['POST'])
-def success():
-    if request.method == 'POST':
-        f = request.files['file']
-        filename = f.filename
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        f.save(filepath)
-        return render_template("Acknowledgement.html", name=filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
